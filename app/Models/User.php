14cf4838becd5838b6a724fb40fc09b73e99e3be
samuelkirbyaguilar <shaguilar@up.edu.ph>
2021-10-15 02:39:13 +0800

    add is_admin, username and profile_picture to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'profile_picture',
     ];
 
     /**
